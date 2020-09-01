fc296f6ec71a60dcc3a3bca1cec34e0bc94fc316
Robrecht Cannoodt <rcannood@gmail.com>
2020-09-01 08:27:21 +0200

    plot ice slightly smaller than the rest

--- R/map_plotter.R
+++ R/map_plotter.R
@@ -18,8 +18,8 @@ plot_terrain_map <- function(tib, alpha = 1) {
   rivers <- get_river_coordinates(tib)
 
   plot_empty_map(tib) +
-    ggforce::geom_regon(aes(fill = terrain_name), data = tib %>% filter((feature_name %|% "") != "Ice"), alpha = alpha) +
-    ggforce::geom_regon(aes(fill = feature_name), data = tib %>% filter(feature_name == "Ice"), alpha = alpha) +
+    ggforce::geom_regon(aes(fill = terrain_name), alpha = alpha) +
+    ggforce::geom_regon(aes(fill = feature_name, r = xy_ratio * .9), data = tib %>% filter(feature_name == "Ice"), alpha = alpha) +
     geom_text(aes(label = "^"), tib %>% filter(terrain_form == "Hill"), alpha = alpha) +
     geom_text(aes(label = "^"), tib %>% filter(terrain_form == "Mountain"), fontface = "bold", alpha = alpha) +
     geom_segment(aes(x = xa, xend = xb, y = ya, yend = yb), colour = feature_palette[["River"]], rivers, size = 1, alpha = alpha) +
